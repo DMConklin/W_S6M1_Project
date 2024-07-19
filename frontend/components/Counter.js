a5b0586f7b0e17bf7832abf1c3e9a4d0afc02203
Darrell Conklin <dmconklin@Darrells-MacBook-Pro.local>
2024-07-18 21:30:43 -0500

    Counter.js step 1 completed - created getter and setter for count using setState with initial value of 0

--- frontend/components/Counter.js
+++ frontend/components/Counter.js
@@ -48,7 +48,7 @@ STEP 6:
 import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
-  /* STEP 1 */
+  const [count, setCount] = useState(0)/* STEP 1 */
 
   const increment = () => {
     /* STEP 4 */
